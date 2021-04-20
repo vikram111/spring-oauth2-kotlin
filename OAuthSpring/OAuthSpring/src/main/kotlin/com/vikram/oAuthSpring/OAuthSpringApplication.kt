8aadb26081c2cba99aee8a052c0bd944db30f70a
vikramthakur <vikram.thakur788@gmail.com>
2021-04-20 22:29:36 +1000

    Further changes

--- OAuthSpring/OAuthSpring/src/main/kotlin/com/vikram/oAuthSpring/OAuthSpringApplication.kt
+++ OAuthSpring/OAuthSpring/src/main/kotlin/com/vikram/oAuthSpring/OAuthSpringApplication.kt
@@ -1,17 +1,16 @@
 package com.vikram.oAuthSpring
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService
-import org.springframework.security.oauth2.core.user.OAuth2User
+import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.authentication.HttpStatusEntryPoint
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 
 @SpringBootApplication
@@ -33,15 +32,17 @@ class OauthSecConfig: WebSecurityConfigurerAdapter() {
 			.exceptionHandling { e ->
 			e.authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
 		}.oauth2Login{e ->
-			e.failureHandler{request, response, exception ->
-				request.session.setAttribute("error.message", exception.message)
-				simpleUrlAuthenticationFailureHandler.onAuthenticationFailure(request,response,exception)
-			}
+			e.failureHandler{request, response, exception -> handleFailure(request, response, exception, simpleUrlAuthenticationFailureHandler)}
 			}
 
 	}
 }
 
+fun handleFailure(request: HttpServletRequest, response: HttpServletResponse, exception: AuthenticationException, handler: SimpleUrlAuthenticationFailureHandler){
+	println("I was called")
+	request.session.setAttribute("error.message", exception.message)
+	handler.onAuthenticationFailure(request,response,exception)
+}
 
 
 
--- OAuthSpring/OAuthSpring/src/main/kotlin/com/vikram/oAuthSpring/UserController.kt
+++ OAuthSpring/OAuthSpring/src/main/kotlin/com/vikram/oAuthSpring/UserController.kt
@@ -9,13 +9,16 @@ import javax.servlet.http.HttpServletRequest
 class UserController {
 
     @GetMapping("/user")
-    fun user(@AuthenticationPrincipal user: OAuth2User): Map<String,Any>{
-        return mapOf("name" to user.name)
+    fun user(@AuthenticationPrincipal user: OAuth2User): Map<String,Any?>{
+        return mapOf("name" to user.attributes.get("name"))
     }
 
    @GetMapping("/error")
-   fun onError(request: HttpServletRequest){
-       val attribute: String  = request.session.getAttribute("error.message") as String
+   @ResponseBody
+   fun onError(request: HttpServletRequest): String?{
+       println("The session data is ${request.session.getAttribute("error.message")}")
+       val attribute: String?  = request.session?.getAttribute("error.message") as? String
        request.session.removeAttribute("error.message")
+       return attribute;
    }
 }
